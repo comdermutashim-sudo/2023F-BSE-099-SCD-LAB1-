66349102db500ff00da61118654feb2cd2c8d31d
comdermutashim-sudo <comdermutashim@gmail.com>
2025-10-27 15:52:03 +0500

    Add files via upload

---
+++ lab1.java
@@ -0,0 +1,13 @@
+public class lab1 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+
+        int sum = sum(num1, num2);
+        System.out.println(sum);
+
+    }
+    static int sum(int num1,int num2){
+        return num1+num2;        
+    }
+}
